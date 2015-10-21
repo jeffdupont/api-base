79e1711bb8e9517f5aece3a2c112ae37865f579a
Jeff Dupont <jeff.dupont@gmail.com>
2015-10-21 13:58:36 -0700

    echo some strings to test with

--- src/Api/Model.php
+++ src/Api/Model.php
@@ -3,5 +3,8 @@ namespace Api;
 
 class Model
 {
-
+    public function __construct()
+    {
+        echo __METHOD__ . "\n";
+    }
 }
--- src/Api/Models/BaseModel.php
+++ src/Api/Models/BaseModel.php
@@ -3,5 +3,10 @@ namespace Api\Models;
 
 class BaseModel extends Api\Model
 {
+    public function __construct()
+    {
+        echo __METHOD__ . "\n";
 
+        parent::__construct();
+    }
 }
